7d12b09be092413e392babfa4835f5683c11cd93
gonzafranco <gonzafrancoandres@gmail.com>
2023-04-25 20:34:58 -0300

    first commit

---
+++ Procesador_matematico_test/UnitTest1.cs
@@ -0,0 +1,149 @@
+using Xunit;
+using primer_parcial_unit_test_procesador_matematico;
+
+namespace Procesador_matematico_test
+{
+    public class UnitTest1
+    {
+     
+         [Fact]
+         public void SumaTest__un_parametro_negativo()
+         {
+
+           ProcesadorMatematico procesadorMatematico = new ProcesadorMatematico();
+
+            int resultadoEsperado = 0;
+
+            procesadorMatematico.Sumar(0, -1);
+            int resultadoReal = procesadorMatematico.getResultado();
+
+            Assert.Equal(resultadoEsperado, resultadoReal);
+
+         }
+
+        [Fact]
+        public void SumaTest__dos_parametro_negativo()
+        {
+
+            ProcesadorMatematico procesadorMatematico = new ProcesadorMatematico();
+
+            int resultadoEsperado = 0;
+
+            procesadorMatematico.Sumar(-1, -1);
+            int resultadoReal = procesadorMatematico.getResultado();
+
+            Assert.Equal(resultadoEsperado, resultadoReal);
+
+        }
+
+
+        [Fact]
+        public void divisionTest__dividir_por_cero()
+        {
+
+            ProcesadorMatematico procesadorMatematico = new ProcesadorMatematico();
+
+            int resultadoEsperado = 0;
+
+            procesadorMatematico.Dividir(12, 0);
+            int resultadoReal = procesadorMatematico.getResultado();
+
+            Assert.Equal(resultadoEsperado, resultadoReal);
+
+        }
+
+        [Fact]
+        public void divisionTest__divisor_negativo()
+        {
+
+            ProcesadorMatematico procesadorMatematico = new ProcesadorMatematico();
+
+            int resultadoEsperado = 0;
+
+            procesadorMatematico.Dividir(12, -1);
+            int resultadoReal = procesadorMatematico.getResultado();
+
+            Assert.Equal(resultadoEsperado, resultadoReal);
+
+        }
+
+        [Fact]
+        public void divisionTest__dividendo_negativo()
+        {
+
+            ProcesadorMatematico procesadorMatematico = new ProcesadorMatematico();
+
+            int resultadoEsperado = 0;
+
+            procesadorMatematico.Dividir(-12, 1);
+            int resultadoReal = procesadorMatematico.getResultado();
+
+            Assert.Equal(resultadoEsperado, resultadoReal);
+
+        }
+
+        [Fact]
+        public void CalcularMayorTest__primer_parametro_mayor()
+        {
+
+            ProcesadorMatematico procesadorMatematico = new ProcesadorMatematico();
+
+            int resultadoEsperado = 12;
+
+            procesadorMatematico.CalcularMayor(12, 0);
+            int resultadoReal = procesadorMatematico.getResultado();
+
+            Assert.Equal(resultadoEsperado, resultadoReal);
+
+        }
+
+        [Fact]
+        public void CalcularMayorTest__segundo_parametro_mayor()
+        {
+
+            ProcesadorMatematico procesadorMatematico = new ProcesadorMatematico();
+
+            int resultadoEsperado = 12;
+
+            procesadorMatematico.CalcularMayor(0, 12);
+            int resultadoReal = procesadorMatematico.getResultado();
+
+            Assert.Equal(resultadoEsperado, resultadoReal);
+
+        }
+
+        [Fact]
+        public void CalcularMayorTest__parametros_Iguales()
+        {
+
+            ProcesadorMatematico procesadorMatematico = new ProcesadorMatematico();
+
+            int resultadoEsperado = 12;
+
+            procesadorMatematico.CalcularMayor(12, 12);
+            int resultadoReal = procesadorMatematico.getResultado();
+
+            Assert.Equal(resultadoEsperado, resultadoReal);
+
+        }
+
+        [Fact]
+        public void CalcularMayorTest__numero_negativo()
+        {
+
+            ProcesadorMatematico procesadorMatematico = new ProcesadorMatematico();
+
+            int resultadoEsperado = 12;
+
+            procesadorMatematico.CalcularMayor(12, -12);
+            int resultadoReal = procesadorMatematico.getResultado();
+
+            Assert.Equal(resultadoEsperado, resultadoReal);
+
+        }
+
+
+
+
+    }
+}
---
+++ primer-parcial-unit-test-procesador-matematico/ProcesadorMatematico.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primer_parcial_unit_test_procesador_matematico
+{
+    public class ProcesadorMatematico
+    {
+        private int _resultado;
+
+        public void Sumar(int primerNumero, int segundoNumero)
+        {
+            this._resultado = primerNumero + segundoNumero;
+            if (this.EsNegativo())
+            {
+                this._resultado = 0;
+            }
+        }
+
+        public void Dividir(int dividendo, int divisor)
+        {
+            if(divisor == 0)
+            {
+                this._resultado = 0;
+                return;
+            }
+
+
+            this._resultado = dividendo / divisor;
+            if (this.EsNegativo())
+            {
+                this._resultado = 0;
+            }
+        }
+        private bool EsNegativo()
+        {
+            return (this._resultado < 0);
+        }
+
+        public void CalcularMayor(int primerNumero, int segundoNumero)
+        {
+            if (primerNumero > segundoNumero)
+            {
+                this._resultado = primerNumero;
+            }
+            else if (primerNumero < segundoNumero)
+            {
+                this._resultado = segundoNumero;
+            }
+            else
+            {
+                this._resultado = segundoNumero;
+            }
+        }
+
+
+        public int getResultado()
+        {
+            return this._resultado;
+        }
+    }
+}
